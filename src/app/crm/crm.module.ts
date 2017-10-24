947b27717203ac505aeac51ba42c681a788e94d2
Kirill <master.salesman88@gmail.com>
2017-10-24 23:13:14 +0300

    finalEditBeforeMergeBranch

--- src/app/crm/crm.module.ts
+++ src/app/crm/crm.module.ts
@@ -4,17 +4,10 @@ import { ModelModule } from '../model/model.module';
 import { CrmComponent } from './crm.component';
 import { NavComponent } from './nav/nav.component';
 import { ContentComponent } from './content/content.component';
-import { ReportListComponent } from './report-list/report-list.component';
-import { ReportDetailComponent } from './report-detail/report-detail.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ModelModule
-  ],
-  declarations: [CrmComponent, NavComponent, ContentComponent, ReportListComponent, ReportDetailComponent],
-  exports: [
-    CrmComponent
-  ]
+  imports: [ CommonModule, ModelModule ],
+  declarations: [CrmComponent, NavComponent, ContentComponent ],
+  exports: [ CrmComponent ]
 })
 export class CrmModule { }
